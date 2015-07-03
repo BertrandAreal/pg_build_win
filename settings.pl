1de603d8681fa2f4f0987d889fb19ae9e723662b
unknown <jenkins@VMJENKINS.starck-areal.local>
2015-07-03 10:21:20 +0200

    add jenkins configuration

---
+++ settings.pl
@@ -0,0 +1,46 @@
+#
+# This file configures the build. Copy it to
+# "settings.pl" and edit it to reflect your needs and
+# environment.
+# 
+
+$cfg = {
+
+	'makeargs' => {
+		'LIBDIR' => 'c:\pg\libs',
+		'TOOLPREFIX' => 'c:',
+
+		#################################### 
+		# Manual PostgreSQL source trees   #
+		####################################
+		#
+		# If you want to install the resulting build, set PGINSTALLDIR
+		# to the location you want to install to. If unset, the default 
+		# will be to install to "binaries\" under the build directory.
+		'PGINSTALLDIR' => undef,
+		
+		##############################################
+		# PostgreSQL automatically managed from git  #
+		##############################################
+		# These settings apply only to buildgit.pl
+		'PGDIR' => 'c:\pg\postgresql',
+		'GIT' => 'c:\Program Files (x86)\Git\bin\git.exe',
+        # Where to fetch changes from
+        'PG_GIT_URL' => 'git://github.com/BertrandAreal/postgres.git',
+        # If you want clones to use a --reference, specify the repo here.
+        #
+        # I strongly recommend using a local cache of the PostgreSQL git repo
+        # e.g. C:\pg\postgresql-git and using Task Scheduler to pull a mirror
+        # of PostgreSQL's master repo on a regular basis. See the README.
+        #
+        #'PG_GIT_REFERENCE' => 'c:\pg\postgresql-git',
+		'PG_BRANCH' => 'master',
+		# The location of git will usually be autodetected, and it'll be found on
+		# the PATH if present. Set it if the scripts don't find it.
+		#'GIT' => undef
+		# if you GIT_PULL to any value in USE_GIT mode then the
+		# working directory will be updated before each build. 
+		'GIT_PULL' => 1,
+		'CONFIGURATION' => 'RELEASE'
+	} # end of @makeargs
+};
---
+++ startJenkins.cmd
@@ -0,0 +1,6 @@
+
+SET PGBW=%~dp0
+SET TA=x64
+SET BT=release
+SET SDK=vs2013ex
+call %PGBW%\jenkins.cmd
